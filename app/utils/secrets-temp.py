8206045e657cf21448664e88de2c9e0b40dc40e0
conchesness <37635142+conchesness@users.noreply.github.com>
2023-01-20 07:23:56 -0800

    edit setup

---
+++ app/utils/secrets-temp.py
@@ -0,0 +1,11 @@
+import os
+
+def getSecrets():
+    secrets = {
+        'MONGO_HOST':'',
+        'MONGO_DB_NAME':'',
+        'GOOGLE_CLIENT_ID': '',
+        'GOOGLE_CLIENT_SECRET':'',
+        'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration"
+        }
+    return secrets
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# This is the file that you run to start your app
 
 from app import app
 import os
@@ -7,4 +6,5 @@ if __name__ == "__main__":
     
     os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
     
-    app.run(debug="True")
+    # app.run(debug="True", ssl_context='adhoc')
+    app.run(debug="True",use_reloader=True, ssl_context=('cert.pem', 'key.pem'))
