54b9058eb1d243f5f3135a6f0d15e952f107d16b
conchesness <37635142+conchesness@users.noreply.github.com>
2023-02-01 06:34:59 -0800

    about us rout deleted

--- app/routes/default.py
+++ app/routes/default.py
@@ -2,10 +2,6 @@ from app import app
 from flask import render_template
 
 # This is for rendering the home page
-@app.route('/aboutus')
-def aboutus():
-    return render_template('aboutus.html')
-
 @app.route('/')
 def index():
     return render_template('index.html')
