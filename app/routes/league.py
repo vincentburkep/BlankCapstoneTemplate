f188e292a37b3e6ca64d2020eee9c3bfe445b615
adamedlin <25aedlin@athenian.org>
2025-04-22 01:35:10 -0700

    all holy lord please

--- app/routes/league.py
+++ app/routes/league.py
@@ -2,26 +2,19 @@ from app import app
 import mongoengine.errors
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user
-from app.classes.data import League, Comment
-from app.classes.forms import LeagueForm, CommentForm
+from app.classes.data import League
+from app.classes.forms import LeagueForm
 from flask_login import login_required
 import datetime as dt
 
 @app.route('/league/new', methods=['GET', 'POST'])
-# This means the user must be logged in to see this page
 @login_required
-# This is a function that is run when the user requests this route.
 def leagueNew():
     # This gets the form object from the form.py classes that can be displayed on the template.
     form = LeagueForm()
 
-    # This is a conditional that evaluates to 'True' if the user submitted the form successfully.
-    # validate_on_submit() is a method of the form object. 
     if form.validate_on_submit():
 
-        # This stores all the values that the user entered into the new blog form. 
-        # Blog() is a mongoengine method for creating a new blog. 'newBlog' is the variable 
-        # that stores the object that is the result of the Blog() method.  
         newLeague = League(
             # the left side is the name of the field from the data table
             # the right side is the data the user entered which is held in the form object.
@@ -67,9 +60,8 @@ def league(leagueID):
     # there is a field on the comment collection called 'blog' that is a reference the Blog
     # document it is related to.  You can use the blogID to get the blog and then you can use
     # the blog object (thisBlog in this case) to get all the comments.
-    theseComments = Comment.objects(league=thisLeague)
     # Send the blog object and the comments object to the 'blog.html' template.
-    return render_template('league.html',league=thisLeague,comments=theseComments)
+    return render_template('league.html',league=thisLeague)
 
 @app.route('/league/list')
 @app.route('/leagues')
@@ -82,4 +74,62 @@ def leagueList():
     # This renders (shows to the user) the blogs.html template. it also sends the blogs object 
     # to the template as a variable named blogs.  The template uses a for loop to display
     # each blog.
+    return render_template('leagues.html',leagues=leagues)
+
+@app.route('/league/edit/<leagueID>', methods=['GET', 'POST'])
+@login_required
+def leagueEdit(leagueID):
+    editLeague = League.objects.get(id=leagueID)
+    # if the user that requested to edit this blog is not the author then deny them and
+    # send them back to the blog. If True, this will exit the route completely and none
+    # of the rest of the route will be run.
+    if current_user != editLeague.author:
+        flash("You can't edit a league you don't own.")
+        return redirect(url_for('league',leagueID=leagueID))
+    # get the form object
+    form = LeagueForm()
+    # If the user has submitted the form then update the blog.
+    if form.validate_on_submit():
+        # update() is mongoengine method for updating an existing document with new data.
+        editLeague.update(
+            name = form.name.data,
+            founder = form.founder.data,
+            sport = form.sport.data,
+            num_of_teams = form.num_of_teams.data,
+            modify_date = dt.datetime.utcnow
+        )
+        # After updating the document, send the user to the updated blog using a redirect.
+        return redirect(url_for('league',leagueID=leagueID))
+
+    # if the form has NOT been submitted then take the data from the editBlog object
+    # and place it in the form object so it will be displayed to the user on the template.
+    form.name.data = editLeague.name
+    form.founder.data = editLeague.founder
+    form.sport.data = editLeague.sport
+    form.num_of_teams.data = editLeague.num_of_teams
+
+
+    # Send the user to the blog form that is now filled out with the current information
+    # from the form.
+    return render_template('leagueform.html',form=form)
+
+@app.route('/league/delete/<leagueID>')
+# Only run this route if the user is logged in.
+@login_required
+def leagueDelete(leagueID):
+    # retrieve the blog to be deleted using the blogID
+    deleteLeague = League.objects.get(id=leagueID)
+    # check to see if the user that is making this request is the author of the blog.
+    # current_user is a variable provided by the 'flask_login' library.
+    if current_user == deleteLeague.author:
+        # delete the blog using the delete() method from Mongoengine
+        deleteLeague.delete()
+        # send a message to the user that the blog was deleted.
+        flash('The League was deleted.')
+    else:
+        # if the user is not the author tell them they were denied.
+        flash("You can't delete a league you don't own.")
+    # Retrieve all of the remaining blogs so that they can be listed.
+    leagues = League.objects()  
+    # Send the user to the list of remaining blogs.
     return render_template('leagues.html',leagues=leagues)
